a3cb9e18e0fa85329e62ef4562a198396faa99d7
Igor Vladimirskiy <letulip@Igors-Mac-mini.local>
2022-08-11 15:34:10 +0300

    feat: add readme exact content, remove v1/ from urls

--- backend/foodgram/foodgram/urls.py
+++ backend/foodgram/foodgram/urls.py
@@ -18,7 +18,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('shoplist.urls')),
-    path('api/v1/', include('api.urls')),
-    path('api/v1/', include('users.urls')),
+    path('api/', include('shoplist.urls')),
+    path('api/', include('api.urls')),
+    path('api/', include('users.urls')),
 ]
